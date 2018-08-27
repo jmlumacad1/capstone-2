d4049b95e829bf9e3a8e3a1c82e6a2d20dc32402
Jerome <27472643+jmlumacad1@users.noreply.github.com>
2018-08-27 11:44:15 +0800

    updates nav link

--- partials/nav.php
+++ partials/nav.php
@@ -2,14 +2,14 @@
 <ul id="dropdown1" class="dropdown-content">
   <li><a href="#!">Profile</a></li>
   <li class="divider"></li>
-  <li><a href="#!">Logout</a></li>
+  <li><a href="logout.php">Logout</a></li>
 </ul>
 <nav>
   <div class="nav-wrapper">
     <a href="index.php" class="brand-logo">Logo</a>
     <ul class="right hide-on-med-and-down">
       <li><a href="#">Link</a></li>
-      <li><a href="#">Link</a></li>
+      <li><a href="cart_list.php">Cart</a></li>
       <!-- Dropdown Trigger -->
       <li><a class="dropdown-trigger dropdown_menu" href="#!" data-target="dropdown1">User<i class="material-icons right">arrow_drop_down</i></a></li>
     </ul>
